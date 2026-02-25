0ea5297b8d09f00ecac30346d8078199a374a59a
sneha-patil412 <81667469+sneha-patil412@users.noreply.github.com>
2026-02-25 19:46:10 +0530

    Update app.py

--- OLA_RIDE_PROJECT/app.py
+++ OLA_RIDE_PROJECT/app.py
@@ -128,22 +128,23 @@ elif page == "Power BI Dashboard":
 
     with tab1:
         st.subheader("Overall Ride Analysis")
-        st.image("OLA_RIDE_PROJECT/overall.png", use_container_width=True)
+        st.image("OLA_RIDE_PROJECT/Overall.png", use_container_width=True)
 
     with tab2:
         st.subheader("Vehicle Performance")
-        st.image("OLA_RIDE_PROJECT/vehicle.png", use_container_width=True)
+        st.image("OLA_RIDE_PROJECT/Vehicle.png", use_container_width=True)
 
     with tab3:
         st.subheader("Revenue & Value Insights")
-        st.image("OLA_RIDE_PROJECT/revenue.png", use_container_width=True)
+        st.image("OLA_RIDE_PROJECT/Revenue.png", use_container_width=True)
 
     with tab4:
         st.subheader("Cancellation Analysis")
-        st.image("OLA_RIDE_PROJECT/cancellation.png", use_container_width=True)
+        st.image("OLA_RIDE_PROJECT/Cancellation.png", use_container_width=True)
 
     with tab5:
         st.subheader("Driver & Customer Ratings")
-        st.image("OLA_RIDE_PROJECT/rating.png", use_container_width=True)
+        st.image("OLA_RIDE_PROJECT/Rating.png", use_container_width=True)
+
 
 
